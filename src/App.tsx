35462220e843ed9ca1460228e1ef8c54ad1a4770
Thang <lhthang96@gmail.com>
2021-01-04 22:10:15 +0700

    feat: setup firebase project and authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Firebase from "firebase";
 
-const App: React.FC = () => <div />;
+const App: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSignIn = async (): Promise<void> => {
+    try {
+      const result = await Firebase.auth().signInWithEmailAndPassword(
+        username,
+        password
+      );
+      console.log("log result : ", result);
+    } catch (error) {
+      console.log("log error: ", error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button onClick={onSignIn}>Sign in</button>
+    </div>
+  );
+};
 
 export default App;
