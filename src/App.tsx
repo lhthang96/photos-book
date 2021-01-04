185cc00a851161338b26e2ce95a55651ecb6892b
Thang <lhthang96@gmail.com>
2021-01-04 22:57:46 +0700

    feat: added firebase storage, done uploading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import Firebase from "firebase";
+import React, { ChangeEvent, useState } from "react";
+import firebase from "firebase/app";
+import "firebase/storage";
 
 const App: React.FC = () => {
   const [username, setUsername] = useState("");
@@ -7,14 +8,39 @@ const App: React.FC = () => {
 
   const onSignIn = async (): Promise<void> => {
     try {
-      const result = await Firebase.auth().signInWithEmailAndPassword(
-        username,
-        password
-      );
+      const result = await firebase
+        .auth()
+        .signInWithEmailAndPassword(username, password);
       console.log("log result : ", result);
     } catch (error) {
       console.log("log error: ", error);
     }
+  };
+
+  const onUploadFile = async (
+    event: ChangeEvent<HTMLInputElement>
+  ): Promise<void> => {
+    const file = event.target.files ? event.target.files[0] : undefined;
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.addEventListener(
+      "load",
+      async (event: ProgressEvent<FileReader>) => {
+        const readFile = event.target?.result;
+        if (!readFile) return;
+
+        const storage = firebase.storage();
+        const storageRef = storage.ref();
+        const fileRef = storageRef.child("test.pdf");
+        const uploadingFile = new File([readFile], "Test upload");
+
+        const snapshot = await fileRef.put(uploadingFile);
+        console.log("log snapshot : ", snapshot);
+      }
+    );
+
+    reader.readAsArrayBuffer(file);
   };
 
   return (
@@ -28,6 +54,10 @@ const App: React.FC = () => {
         onChange={(event) => setPassword(event.target.value)}
       />
       <button onClick={onSignIn}>Sign in</button>
+      <div>
+        <input type="file" multiple={false} onChange={onUploadFile} />
+        <button>Upload</button>
+      </div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import firebase from "firebase";
+import firebase from "firebase/app";
 
 const firebaseApiKey = process.env.REACT_APP_FIREBASE_API_KEY;
 const firebaseAppId = process.env.REACT_APP_FIREBASE_APP_ID;
