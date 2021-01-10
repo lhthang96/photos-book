4b5c15107ebe8b00b0956e33ed31ade737c7edab
Thang <lhthang96@gmail.com>
2021-01-10 10:57:31 +0700

    feat: added path alias for tsconfig, created post story page

---
+++ src/pages/DevData/PostStory/PostStory.tsx
@@ -0,0 +1,54 @@
+import { Input, DatePicker, Form, Button } from 'antd';
+import React from 'react';
+import moment from 'moment';
+import { dbPushStory } from 'src/services/database';
+import { StyledDevData, StyledPostStoryForm } from './PostStory.styles';
+import { PostStoryPayload } from 'src/common/interfaces';
+import { getCurrentUser } from 'src/common/utils';
+
+const { TextArea } = Input;
+
+export const PostStory: React.FC = () => {
+  const onFinishForm = async (formValues: any): Promise<void> => {
+    const currentUser = getCurrentUser();
+    if (!currentUser) return;
+
+    const newDiary: PostStoryPayload = {
+      uid: currentUser.uid,
+      title: formValues.title,
+      isPrivate: false,
+      date: moment(formValues.date).toISOString()
+    };
+
+    try {
+      await dbPushStory(newDiary);
+    } catch (error) {
+      console.log('log error : ', error);
+    }
+  };
+
+  return (
+    <StyledDevData>
+      <StyledPostStoryForm onFinish={onFinishForm}>
+        <Form.Item name="date">
+          <DatePicker style={{ marginBottom: 24 }} />
+        </Form.Item>
+
+        <Form.Item name="title">
+          <Input placeholder="Diary title" style={{ marginBottom: 24 }} />
+        </Form.Item>
+
+        <Form.Item name="content">
+          <TextArea
+            autoSize={{ minRows: 4, maxRows: 8 }}
+            style={{ marginBottom: 24 }}
+          />
+        </Form.Item>
+
+        <Button type="primary" htmlType="submit">
+          Post
+        </Button>
+      </StyledPostStoryForm>
+    </StyledDevData>
+  );
+};
